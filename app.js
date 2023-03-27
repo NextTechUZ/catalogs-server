7db63f43133934d2ae61cc7ed135119905363d02
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-03-27 10:19:27 +0500

    bug fixed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const orderRoutes = require("./routers/orderRouter");
+
 const userRoutes = require("./routers/userRouter");
 
 const app = express();
@@ -9,6 +9,21 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/users", userRoutes);
-app.use("/orders", orderRoutes);
 
 module.exports = app;
+
+// PASSWORD = XVAr6bZCzJWHZrFl
+// DATABASE = mongodb+srv://fazliddin:<password>@cluster0.2vmvbt7.mongodb.net/?retryWrites=true&w=majority
+// APP_URL = localhost:3000
+
+// JWT_SECRET = thiswisjwtsecret
+// JWT_EXPIRES = 90d
+
+// const t = {
+//   images: string[],
+//   titleRu: string,
+//   titleUz: string,
+//   descriptionRu: string,
+//   descriptionUz: string,
+//   about:string
+// };
