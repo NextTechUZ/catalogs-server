e9885619c4218e87e8fb8cd92acaeb7829ca9494
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-04-27 18:02:23 +0500

    bug fixed

--- src/routers/mediaRouter.js
+++ src/routers/mediaRouter.js
@@ -12,12 +12,15 @@ const {
 
 const mediaRoutes = express.Router();
 
-mediaRoutes.route("/").get(getAllMedia).post(uploadMulti, createMedia);
+mediaRoutes
+  .route("/")
+  .get(getAllMedia)
+  .post(routeProtector, uploadMulti, createMedia);
 
 mediaRoutes
   .route("/:id")
   .get(getMedia)
-  .patch(uploadSingle, editMedia)
-  .delete(deleteMedia);
+  .patch(routeProtector, uploadSingle, editMedia)
+  .delete(routeProtector, deleteMedia);
 
 module.exports = mediaRoutes;
